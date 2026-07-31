8ced8dd81b13f4d2e6cde0ed88bdf7ab0d71ab4a
denis-komarovs <deniskomarovs.dev@gmail.com>
2026-07-31 15:53:07 +0100

    fix: use root base path for custom domain

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/happy-birthday-isabel/",
+  base: "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
